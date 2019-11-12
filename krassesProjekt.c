8dce9ad206a716467e8109afacf84ea546ce5cf4
Jannis Berndt <berndtjannis@gmail.com>
2019-11-12 09:44:35 +0100

    initial project version

---
+++ krassesProjekt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void zeichneBaumkrone(int groesse) {
+    
+}
+
+void zeichneBaumstamm(int groesse) {
+    
+}
+
+int main(void) {
+    int groesse = 11;
+    zeichneBaumkrone(groesse);
+    zeichneBaumstamm(groesse);
+    return 0;
+}
