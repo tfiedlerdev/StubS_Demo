261794d3e3cf00f58f565a89ee0ea15c34925b5d
Jannis Berndt <berndtjannis@gmail.com>
2019-11-12 09:46:58 +0100

    Baumkrone implementiert

--- krassesProjekt.c
+++ krassesProjekt.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
 void zeichneBaumkrone(int groesse) {
-    
+    for(int row = 0; row < groesse; row++) {
+        for(int j = row; j < groesse; j++) {
+            printf(" ");
+        }
+        for(int j = 0; j < row; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
 }
 
 void zeichneBaumstamm(int groesse) {
